9334da7ac757f98c798a13b248dff87aeb151a78
darjanb5 <darjanbozic4@gmail.com>
2024-06-03 21:24:51 +0200

    Add files via upload

---
+++ kosarkaKviz-main/kosarkaSolution/kosarkaProject/file_utils.h
@@ -0,0 +1,7 @@
+#ifndef FILE_UTILS_H
+#define FILE_UTILS_H
+
+void kopirajDatoteku(const char* izvornaDatoteka, const char* odredisnaDatoteka);
+void obrisiDatoteku(const char* nazivDatoteke);
+
+#endif 
