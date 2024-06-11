710f4aa3c51ff233314c2f86c0e426bb5e9593b4
darjanb5 <darjanbozic4@gmail.com>
2024-06-11 22:03:32 +0200

    Delete kosarkaKviz-main/kosarkaSolution directory

--- kosarkaKviz-main/kosarkaSolution/kosarkaProject/file_utils.h
+++
@@ -1,7 +0,0 @@
-#ifndef FILE_UTILS_H
-#define FILE_UTILS_H
-
-void kopirajDatoteku(const char* izvornaDatoteka, const char* odredisnaDatoteka);
-void obrisiDatoteku(const char* nazivDatoteke);
-
-#endif 
